7b0eb2b5f69f4bbb11b464582612a0f043b57a87
yutitakagi <takagiyuri0712@gmail.com>
2022-10-18 14:16:34 +0900

    add: rails db:migrateを実行

--- db/migrate/20221018050928_add_column_type_to_events.rb
+++ db/migrate/20221018050928_add_column_type_to_events.rb
@@ -1,5 +1,5 @@
 class AddColumnTypeToEvents < ActiveRecord::Migration[6.1]
   def change
-    add_column :events, :type, :string, default: 0, :null: false
+    add_column :events, :type, :string, default: 0, null: false
   end
 end
